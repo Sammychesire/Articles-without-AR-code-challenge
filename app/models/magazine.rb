2f31052758a93a06f49ce83b6246efe7551f674a
bob <sammy.chesire@student.moringaschool.com>
2022-10-31 15:48:34 +0300

    completed challenge

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,11 +1,44 @@
 class Magazine
   attr_accessor :name, :category
 
-
+@@all = []
   def initialize(name, category)
     @name = name
     @category = category
+    @@all << self
 
+  end
+
+  def self.all
+    @@all
+  end
+
+  def mag_articles
+    Article.all.filter{|article|
+  article.magazine.name==@name}
+  end
+
+  def mag_authors
+    mag_articles.map{|article| article.author}.uniq
+  end
+
+  def contributors
+    mag_authors
+  end
+
+  def self.find_by_name(name)
+    Magazine.all.find{|magazine| magazine.name == name}
+  end
+
+  def article_titles
+    mag_articles.map{|article| article.title}
+  end
+
+  def contributing_authors 
+    major_authors = []
+    mag_authors.tally.each{|key,value
+  value > 2 && (major_authors << key)}
+  major_authors
   end
 
 
--- tools/console.rb
+++ tools/console.rb
@@ -7,7 +7,16 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
-
+a1 = Author.new("Author1")
+mag = Magazine.new("mag1","news")
+mag2 = Magazine.new("mag2","news")
+mag3 = Magazine.new("mag3","sports")
+art = Article.new(a1,mag,"Title1")
+a2 = Author.new("Author2")
+art2 = Article.new(a2,mag,"Title2")
+a1.add_article(mag,"Title3")
+a1.add_article(mag2,"Title4")
+a1.add_article(mag3,"Title5")
 
 
 
